e361eb07505ee9acfb4863e5077bf411abbbbebf
bloombar <amos@cs.nyu.edu>
2021-10-26 15:46:28 -0400

    updating with login/logout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import PrimaryNav from './PrimaryNav'
-import Home from './Home'
-import About from './About'
-import AnimalsList from './AnimalsList'
-import Animal from './Animal'
-import './App.css'
+import React, { useState } from "react"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import PrimaryNav from "./PrimaryNav"
+import Home from "./Home"
+import About from "./About"
+import AnimalsList from "./AnimalsList"
+import Animal from "./Animal"
+import Login from "./Login"
+import Logout from "./Logout"
+import "./App.css"
 
 // this is used in one of the routes below for a specific animal with a specific id
 // there's no nicer way to pass props to a component in a route
@@ -19,25 +21,39 @@ const AnimalWrapper = ({ match }) => {
 }
 
 // set up routes so different URL routes load up different main components
-const App = (props) => {
+const App = props => {
+  const [user, setUser] = useState({}) // a state variable that stores the logged-in user, if any
+
   return (
     <div className="container">
       <Router>
         <Switch>
           <Route path="/about">
-            <PrimaryNav />
+            <PrimaryNav user={user} setuser={setUser} />
             <About />
           </Route>
 
           <Route path="/animals/:id" component={AnimalWrapper} />
 
           <Route path="/animals">
-            <PrimaryNav />
+            <PrimaryNav user={user} setuser={setUser} />
             <AnimalsList />
           </Route>
 
+          <Route path="/login">
+            {/* pass the setter function that can be called if the user successfully logs in from the login screen */}
+            <PrimaryNav user={user} setuser={setUser} />
+            <Login user={user} setuser={setUser} />
+          </Route>
+
+          <Route path="/logout">
+            {/* pass the setter function that can be called if the user successfully logs in from the login screen */}
+            <PrimaryNav user={user} setuser={setUser} />
+            <Logout user={user} setuser={setUser} />
+          </Route>
+
           <Route path="/">
-            <PrimaryNav />
+            <PrimaryNav user={user} setuser={setUser} />
             <Home />
           </Route>
         </Switch>
--- src/PrimaryNav.js
+++ src/PrimaryNav.js
@@ -1,13 +1,26 @@
-import React from 'react'
-import './PrimaryNav.css'
-import { Link } from 'react-router-dom'
+import React from "react"
+import "./PrimaryNav.css"
+import { Link } from "react-router-dom"
 
-const PrimaryNav = (props) => {
+const PrimaryNav = props => {
+  // we assume a function named setuser is passed as a prop to this component
+
+  // show a login link if the user is not yet logged in
+  let logInOutComponent = <Link to="/login">Login</Link>
+  // show a logout link if the user is already logged in
+  if (props.user.success)
+    logInOutComponent = (
+      <>
+        <Link to="/logout">Logout {props.user.username}</Link>
+      </>
+    )
+
   return (
     <nav>
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
       <Link to="/animals">Animals</Link>
+      {logInOutComponent}
     </nav>
   )
 }
