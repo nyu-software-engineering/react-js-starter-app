8558acbbfbda243166a3f44493f140a43b51a459
bloombar <2209098+bloombar@users.noreply.github.com>
2026-02-09 19:21:47 -0500

    remoe plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import viteTsconfigPaths from 'vite-tsconfig-paths'
 import svgr from 'vite-plugin-svgr'
-import { ghPages } from 'vite-plugin-gh-pages'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -18,7 +17,6 @@ export default defineConfig(({ mode }) => {
       svgr({
         include: '**/*.svg?react',
       }),
-      ghPages({ branch: 'gh-pages' }), // sets base path to be project name in package.json
     ],
     // overriding default build location for legacy create-react-app reasons in this project
     build: {
